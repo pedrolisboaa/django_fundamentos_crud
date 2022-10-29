75d454a1ebb9e4324bb9619fef10041fa01561db
pedrolisboaa <phlisboa2000@gmail.com>
2022-10-29 00:17:11 -0300

    Criando o template BASE para que seja utilizados como base para outros arquivos HTML.

--- nutricionista/views.py
+++ nutricionista/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+
 from .models import Nutricionista
 from .form import PacienteForm
 
@@ -6,13 +8,22 @@ from .form import PacienteForm
 
 
 def listar_paciente(request):
+    # É Criado uma váriavel que recebe todos os pacientes do banco
     pacientes = Nutricionista.objects.all()
     return render(request, 'nutricionista/listar_pacientes.html', {'pacientes': pacientes})
 
 
 def inserir_paciente(request):
-    form = PacienteForm()
+    # Para inserir um cliente é necessário verificar que o request é do metodo POST, caso ele seja criar uma
+    # instancia do formulário, se ele for válido salvar. Caso não seja será gerada uma istáncia vazia do formulário.
+    if request.method == 'POST':
+        form = PacienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PacienteForm()
     return render(request, 'nutricionista/form_paciente.html', {'form': form})
+
 
 
 
